986b70dbe4b7e9befd129e8d63bcd0ff928cae6e
AngelofDeath666 <s164937@student.dtu.dk>
2022-02-10 12:03:25 +0100

    this almost works

--- src/com/company/week1/peak1.java
+++ src/com/company/week1/peak1.java
@@ -1,4 +1,29 @@
 package com.company.week1;
 
+import java.util.Scanner;
+
 public class peak1 {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        int len = scanner.nextInt();
+        String arr = scanner.nextLine();
+        int[] A = new int[len];
+        int result =0;
+        String[] array = arr.split("\\|");
+        for (int i = 0; i < arr.length()+1;i++) {
+            A[i] = Integer.parseInt(array[i]);
+        }
+
+        if (A[0] >= A[1]) result = 0;
+        for (int j = 1; j < len-1;j++){
+            if (A[j-1] <= A[j] && A[j] >= A[j+1]) result = j;
+
+        }
+        if (A[A.length-2] <= A[A.length-1]) result = A.length-1;
+
+        System.out.println(result);
+
+    }
 }
